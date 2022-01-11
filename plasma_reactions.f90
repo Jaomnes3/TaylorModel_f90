5968dba3ff986dc941b151911187809527c00ead
Jaomnes3 <54838877+Jaomnes3@users.noreply.github.com>
2022-01-12 02:02:25 +0900

    Add files via upload

--- plasma_reactions.f90
+++ plasma_reactions.f90
@@ -6,6 +6,12 @@ subroutine plasma_reactions
 
 !Resting Platelets
 
+ap(i,j) = ap(i,j) + dt*( &
+          -(u(i,j)*(ap(i+1,j)-ap(i,j))/dx + v(i,j)*(ap(i,j+1)-ap(i,j))/dy) &  ! Convection term
+          + Dap*((ap(i+1,j)+ap(i-1,j)-2*ap(i,j))/(dx*dx) &
+               + (ap(i,j+1)+ap(i,j-1)-2*ap(i,j))/(dy*dy)) & ! Diffusion term
+          + kpa*rp(i,j))                   ! Reaction term
+
 !Activated Platelets
 
 !Platelet-Released Agonist (ADP)
